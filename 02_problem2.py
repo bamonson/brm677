8d996f651e4bc11365209a9553ee9cdd24a7c85f
Brett Allen Monson <brettallenmonson@dhcp-10-250-206-221.harvard.edu>
2020-02-04 15:48:27 -0500

    add 02_problem1.py

---
+++ 02_problem2.py
@@ -0,0 +1,9 @@
+#Code to determine the value difference between 2 variables.
+a=3
+b=4
+#Variables "a" and "b"" have values assigned of 3 and 4.
+if a>b:
+	print('a is greater than b')
+else:
+	print('a is not greater than b')
+#The commands of "if" and "else" are used to show if "a" is greater or not than "b".
