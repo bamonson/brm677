8a9a65a5db80ae7133712e1464f88570d8cc956a
Brett Allen Monson <brettallenmonson@dhcp-10-250-206-221.harvard.edu>
2020-02-04 15:49:32 -0500

    add 02_problem3.py

---
+++ 02_problem3.py
@@ -0,0 +1,11 @@
+#Code to determine the value difference between 2 variables.
+a=5
+b=6
+#Variables "a" and "b"" have values assigned of 5 and 6.  
+if a>b:
+	print('a is greater than b')
+elif a<b:
+	print('a is smaller than b')
+else:
+	print('a is equal to b')
+#The commands of "if", "elif", and "else" are used to show if "a" is greater, smaller, or equal to "b".
