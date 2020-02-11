1f7b6b8465041eba9e5db5d41012b6168c201bc3
Brett Allen Monson <brettallenmonson@dhcp-10-250-92-222.harvard.edu>
2020-02-11 18:32:35 -0500

    adding 03_problem5.py

---
+++ 03_problem5.py
@@ -0,0 +1,21 @@
+my_set = {"Brett", "Monson"}
+print(my_set)
+my_set.add("Brett")
+print(my_set)
+
+print("done!")
+# Problem 5: Copy the code from the previous slide in PyCharm
+# and create a set my_set containing 2 strings corresponding
+# to your first name and your last name.
+# Print all the elements.
+# Add your first name again and print all the elements.
+# Describe what you notice.
+# Explain why this happens.
+# Submit screenshots of every step of your debugging session
+# and a screenshot of your script rendered in GitHub.
+
+# Rubric (20 points)
+# 5 points for showing the correct files
+# 5 points for showing the correct code
+# 5 points for showing the correct description
+# 5 points for showing the correct explanation
