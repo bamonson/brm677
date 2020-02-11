7a7006e4e7940f786ef2943b5bb9a215369582e1
Brett Allen Monson <brettallenmonson@dhcp-10-250-92-222.harvard.edu>
2020-02-11 18:32:14 -0500

    adding 03_problem4.py

---
+++ 03_problem4.py
@@ -0,0 +1,19 @@
+my_tuple = ("Brett", "Monson")
+print(my_tuple)
+print(my_tuple[0])
+print(my_tuple[-1])
+print(my_tuple[1:3])
+print(my_tuple[-3:-1])
+
+print("done")
+# Problem 4: Copy the code from the previous slide in PyCharm
+# and create a tuple my_tuple containing 2 strings
+# corresponding to your first name and your last name.
+# Submit screenshots of every step of your debugging session
+# and a screenshot of your script rendered in GitHub.
+
+# Rubric (20 points):
+# 5 points for submitting the correct files
+# 5 points for the correct answers in your table
+# 5 points for showing the correct usage of the debugger
+# 5 points for showing the correct usage of GitHub
