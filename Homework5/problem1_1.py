24eae1f98a31b544435842c287b78fa773f2147a
Brett Allen Monson <brettallenmonson@Bretts-MacBook-Air.local>
2020-03-11 14:56:34 -0400

    problem1_1.py

---
+++ Homework5/problem1_1.py
@@ -0,0 +1,7 @@
+"""imports code to print the number of bases in ecoli.fasta in the terminal."""
+import problem1
+#The import allows us to gain the code from the assigned file.
+count_1=problem1.counting("ecoli.fasta")
+
+print(count_1)
+#This will print the defined output.
